4636984007a610077fca0bc67dacced5671f7135
luiscarlos27 <lennzoferrari@hotmail.com>
2024-07-10 18:14:53 -0500

    Actualizacion del codigo

--- app/helpers/Helper.php
+++ app/helpers/Helper.php
@@ -1,5 +1,6 @@
 <?php
 namespace helpers;
+require_once __DIR__ . '/../models/QueryInnerjoin.php';
 
 class Helper{
     // Método para verificar Encriptar contraseña
@@ -18,6 +19,25 @@ class Helper{
         }
         return $data;
     }
+
+    //Metodo de Formateto json para Conetnido
+    public static function FormatJson($datos){
+        $jsonColumns = ['contenido_sobre', 'nuestro_servicio', 'portafolio'];
+        foreach ($jsonColumns as $column) {
+            if (isset($datos[$column])) {
+                $datos[$column] = json_encode($datos[$column]);
+            }
+        }
+        return $datos;
+    }
+
+public static function BuilQuery($data, $name){
+    global $Querycontenido;
+    // Construir la consulta utilizando $data si es necesario
+    // Por ejemplo, podrías concatenar $data a la consulta si necesitas filtrar por algún campo
+    $query = $Querycontenido . " = " . $data[$name];
+    return $query;
+}
 }
 
 ?>
