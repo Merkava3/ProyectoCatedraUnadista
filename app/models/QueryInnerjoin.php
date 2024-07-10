0057b552a5990a015118bf786e0d9a010b00bad0
luiscarlos27 <lennzoferrari@hotmail.com>
2024-07-10 17:21:27 -0500

    se lo agrego una consulta hacia la base de datos

--- app/models/QueryInnerjoin.php
+++ app/models/QueryInnerjoin.php
@@ -1,3 +1,4 @@
 <?php
 $UserQuerySesion = "select fecha_sesion, estado, usuario.indentificacion, usuario.nombre, usuario.apellido, usuario.correo, usuario.tipo_usuario from sesion inner join usuario on sesion.sesion_usuario = usuario.id_usuario where usuario.tipo_usuario = 'Tutor'";
+$Querycontenido = "select contenido_sobre,nuestro_servicio,portafolio from contenido WHERE id_usuario_contenido";
 ?>
